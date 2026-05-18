ee252e857cc4795abd1949a3241479126716873f
Dao Trong Dinh <trongdinhh1605@gmail.com>
2026-05-18 09:41:38 +0700

    Add initial Java Swing project

---
+++ src/InternshipManagementSystem/src/app/Main.java
@@ -0,0 +1,14 @@
+package app;
+
+import javax.swing.JOptionPane;
+
+public class Main {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(
+                null,
+                "Khởi động hệ thống quản lý thực tập thành công!",
+                "Internship Management System",
+                JOptionPane.INFORMATION_MESSAGE
+        );
+    }
+}
